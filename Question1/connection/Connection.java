6dd79c0ec434ff2c3f991ba2ef5e2ed013e1ec45
Bakatastic <Bakatastic@hotmail.com>
2017-10-07 14:36:39 -0400

    Server breaks loop after connection

--- Question1/connection/Connection.java
+++ Question1/connection/Connection.java
@@ -5,16 +5,18 @@ public class Connection {
     public static void main(String[] args) {
         try
         {
+            //Connect to server socket
             Socket sock = new Socket("127.0.0.1", 6013);
             
             InputStream in = sock.getInputStream();
             BufferedReader bin = new BufferedReader(new InputStreamReader(in));
-            
+            //Read stream from server
             String line;
             while ((line = bin.readLine())!= null)
             {
                 System.out.println(line);
-            }           
+            }
+            //Close connection with server
             sock.close();
         }
         catch(IOException ioe)
--- Question1/reader/readSocket.java
+++ Question1/reader/readSocket.java
@@ -1,4 +1,3 @@
-
 package reader;
 
 import java.io.*;
@@ -8,20 +7,25 @@ public class readSocket {
     public static void main(String[] args) {
         try {
             ServerSocket sock = new ServerSocket(6013);
+            //Listen for connections
             while (true) {
                 Socket client = sock.accept();
-                
+                //Get input stream
                 InputStream in = client.getInputStream();
-
                 System.out.println(in.toString());
-
+                //Output stream to client
                 PrintWriter pout = new PrintWriter(client.getOutputStream(),true);
-
                 pout.println(in.toString());
                 pout.println(client.toString());
-                client.close();       
-            }      
-        } 
+                //Checks to see if connection exists
+                //If exists, close connection and break loop
+                if(sock.isBound()){
+                    System.out.println("Bound!");
+                    client.close();
+                    break;
+                }
+            }
+        }
         catch(IOException ioe){
             System.err.println(ioe);
         }
